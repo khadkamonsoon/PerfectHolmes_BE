cd6b9f283a6228d8d9c719916b0169d06e8ef42d
dhwpdnr <ink1237@gmail.com>
2023-12-11 11:19:30 +0900

    [feat] api routing

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .apis import LoginAPI
+from .apis import LoginAPI, SignupAPI, LogoutAPI, RefreshAPI
 
 urlpatterns = [
     path("login/", LoginAPI.as_view(), name="login"),
+    path("signup/", SignupAPI.as_view(), name="signup"),
+    path("logout/", LogoutAPI.as_view(), name="logout"),
+    path("refresh/", RefreshAPI.as_view(), name="refresh"),
 ]
