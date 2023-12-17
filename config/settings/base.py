eee9d2e706eac5685b7531ad91e6f668e81597c3
dhwpdnr <ink1237@gmail.com>
2023-12-18 00:40:17 +0900

    [setting] edit settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -94,10 +94,6 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
