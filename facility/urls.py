29eb7278bd53840434a8758b1b10b8b72510d7aa
dhwpdnr <ink1237@gmail.com>
2024-04-10 19:29:36 +0900

    [feat] facility list api

--- facility/urls.py
+++ facility/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .apis import AroundFacilityListAPI
+from .apis import AroundFacilityListAPI, FacilityListAPI
 
 urlpatterns = [
+    path("", FacilityListAPI.as_view()),
     path("around", AroundFacilityListAPI.as_view(), name="facility-around"),
 ]
