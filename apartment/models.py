7610f4ffe044cf19c62758d964091617a23bdbe5
dhwpdnr <ink1237@gmail.com>
2024-05-15 18:13:17 +0900

    [feat] apartment model

--- apartment/models.py
+++ apartment/models.py
@@ -9,6 +9,11 @@ class Apartment(CommonModel):
     address = models.CharField(max_length=100, verbose_name="주소")
     lat = models.FloatField(verbose_name="위도")
     lng = models.FloatField(verbose_name="경도")
+    room_count = models.IntegerField(verbose_name="방 개수", default=1)
+    area = models.FloatField(verbose_name="평수", default=1)
+    built_year = models.IntegerField(verbose_name="준공년도", default=2020)
+    price = models.IntegerField(verbose_name="가격", default=0)
+    households = models.IntegerField(verbose_name="세대수", default=1)
 
     def __str__(self):
         return self.name
