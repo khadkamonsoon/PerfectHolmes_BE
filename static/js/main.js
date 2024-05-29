5cca903d45abf72ecde63c75ab6dde52d1fcbebb
monsoon khadka <monsoonkhadka8@gmail.com>
2024-05-30 00:43:20 +0900

    show detail on side

--- static/js/main.js
+++ static/js/main.js
@@ -1,175 +1,399 @@
 var map;
 var circle;
 var marker;
-var facilityMarkers = []; // 기존 마커들을 담는 배열
+var facilityMarkers = [];
 var clickedMarker;
 
-// 목포를 기본 지도로
+var places = [
+  {
+    name: "나무숲",
+    subtitle: "카페",
+    description: "빙수개커욘",
+    imageUrl:
+      "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDAyMDhfMTU1%2FMDAxNzA3MzU1NzA0NTIz.Lfj4DJMS9bqkRRXPTXMtDf91yZTuupGt6wO-YbfN47Yg.Lq54OmwRmYGZjhhzTWf4Hm1F5esR5lmsureDZZhLrVUg.JPEG.bdsea1217%2FIMG_7409.jpg&type=f&size=680x360&quality=80&opt=2",
+  },
+  {
+    name: "오늘은 왠지",
+    subtitle: "카페",
+    description: "커피맛이 너무 고급스러워요, 목대 커피는 owen",
+    imageUrl:
+      "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20221020_229%2F1666253513669TeKUY_JPEG%2F94E54031-7DD6-4B42-AE98-5511F3224B50.jpeg",
+  },
+  {
+    name: "메가커피",
+    subtitle: "카페",
+    description:
+      "위치도 좋고 커피도 맛있는 이곳, 패스오더 서비스 도입되면 더 자주 방문할거같아요",
+    imageUrl:
+      "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210206_250%2F1612575043521ah9D6_JPEG%2FMfZhDYNAK4n0KZPidaoJgc_b.jpg",
+  },
+  {
+    name: "나무숲",
+    subtitle: "카페",
+    description: "빙수개커욘",
+    imageUrl:
+      "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDAyMDhfMTU1%2FMDAxNzA3MzU1NzA0NTIz.Lfj4DJMS9bqkRRXPTXMtDf91yZTuupGt6wO-YbfN47Yg.Lq54OmwRmYGZjhhzTWf4Hm1F5esR5lmsureDZZhLrVUg.JPEG.bdsea1217%2FIMG_7409.jpg&type=f&size=680x360&quality=80&opt=2",
+  },
+];
+
 function selectMapList() {
-    if (map) {
-        map.destroy();
-    }
-    map = new naver.maps.Map('map', {
-        center: new naver.maps.LatLng(34.8026, 126.3898),
-        zoom: 14,
-    });
+  if (map) {
+    map.destroy();
+  }
+  map = new naver.maps.Map("map", {
+    center: new naver.maps.LatLng(34.8026, 126.3898),
+    zoom: 14,
+  });
 
-    // 지도를 클릭했을 때 이벤트 등록
-    naver.maps.Event.addListener(map, 'click', function (e) {
-        var latlng = e.coord;
-        updateMarker(latlng);
+  naver.maps.Event.addListener(map, "click", function (e) {
+    var latlng = e.coord;
+    updateMarker(latlng);
 
-        removeFacilityMarkers();
-        clickedMarker = null;
-    });
+    removeFacilityMarkers();
+    clickedMarker = null;
+  });
 }
 
-// 지도를 그려주는 함수
-selectMapList();
-
-// 검색한 주소의 정보를 insertAddress 함수로
 function searchAddressToCoordinate(address) {
-    naver.maps.Service.geocode({
-        query: address
-    }, function (status, response) {
-        if (status === naver.maps.Service.Status.ERROR) {
-            return alert('Something Wrong!');
-        }
-        if (response.v2.meta.totalCount === 0) {
-            return alert('올바른 주소를 입력해주세요.');
-        }
+  naver.maps.Service.geocode(
+    {
+      query: address,
+    },
+    function (status, response) {
+      if (status === naver.maps.Service.Status.ERROR) {
+        return alert("Something Wrong!");
+      }
+      if (response.v2.meta.totalCount === 0) {
+        return alert("올바른 주소를 입력해주세요.");
+      }
 
-        var htmlAddresses = [],
-            item = response.v2.addresses[0],
-            latlng = new naver.maps.LatLng(item.y, item.x);
+      var item = response.v2.addresses[0];
+      var latlng = new naver.maps.LatLng(item.y, item.x);
 
-        if (item.roadAddress) {
-            htmlAddresses.push('[도로명 주소] ' + item.roadAddress);
-        }
-        if (item.jibunAddress) {
-            htmlAddresses.push('[지번 주소] ' + item.jibunAddress);
-        }
-        if (item.englishAddress) {
-            htmlAddresses.push('[영문명 주소] ' + item.englishAddress);
-        }
-
-        insertAddress(item.roadAddress, latlng);
-    });
+      insertAddress(item.roadAddress, latlng);
+    }
+  );
 }
 
-// 주소 검색 버튼 이벤트
-$('#address').on('keydown', function (e) {
-    var keyCode = e.which;
-    if (keyCode === 13) {
-        searchAddressToCoordinate($('#address').val());
-    }
+$("#address").on("keydown", function (e) {
+  var keyCode = e.which;
+  if (keyCode === 13) {
+    searchAddressToCoordinate($("#address").val());
+  }
 });
-$('#submit').on('click', function (e) {
-    e.preventDefault();
-    console.log("test");
-    searchAddressToCoordinate($('#address').val());
+
+$("#submit").on("click", function (e) {
+  e.preventDefault();
+  console.log("test");
+  searchAddressToCoordinate($("#address").val());
 });
 
 naver.maps.onJSContentLoaded = selectMapList;
 
-// 검색한 주소 지도에 마커 표시
 function insertAddress(address, latlng) {
-    updateMarker(latlng);
-    map.setCenter(latlng);
-    map.setZoom(15);
-    sendCoordinatesToServer(latlng.y, latlng.x);
+  updateMarker(latlng);
+  map.setCenter(latlng);
+  map.setZoom(15);
+  sendCoordinatesToServer(latlng.y, latlng.x);
 }
 
-
-// 마커 업데이트
 function updateMarker(latlng) {
-    // 기존 마커와 반경 초기화
-    if (circle) {
-        circle.setMap(null);
-    }
+  if (circle) {
+    circle.setMap(null);
+  }
 
-    if (marker) {
-        marker.setMap(null);
-    }
+  if (marker) {
+    marker.setMap(null);
+  }
 
-    marker = new naver.maps.Marker({
-        map: map,
-        position: latlng,
-    });
+  marker = new naver.maps.Marker({
+    map: map,
+    position: latlng,
+  });
 
-    // 반경 생성
-    circle = new naver.maps.Circle({
-        map: map,
-        center: latlng,
-        radius: 500, // 반경 500m
-        fillColor: '#4285F4',
-        fillOpacity: 0.2,
-        strokeColor: '#4285F4',
-        strokeOpacity: 0.5,
-        strokeWeight: 2,
-    });
+  circle = new naver.maps.Circle({
+    map: map,
+    center: latlng,
+    radius: 600,
+    fillColor: "#4285F4",
+    fillOpacity: 0.2,
+    strokeColor: "#4285F4",
+    strokeOpacity: 0.5,
+    strokeWeight: 2,
+  });
 
-    sendCoordinatesToServer(latlng.y, latlng.x);
+  sendCoordinatesToServer(latlng.y, latlng.x);
 }
 
-
-// 좌표 전송
 function sendCoordinatesToServer(lat, lng) {
-    $.ajax({
-        url: '/facility/around',
-        method: 'GET',
-        data: {
-            lat: lat,
-            lng: lng
-        },
-        success: function (response) {
-            // 기존 마커들 제거
-            removeFacilityMarkers();
-            if (Array.isArray(response) && response.length > 0) {
-                for (var i = 0; i < response.length; i++) {
-                    var facility = response[i];
-                    var facilityLatlng = new naver.maps.LatLng(facility.lat, facility.lng);
-                    var facilityMarker = new naver.maps.Marker({
-                        map: map,
-                        position: facilityLatlng,
-                        title: facility.name
-                    });
-                    // 새로운 마커를 배열에 추가
-                    facilityMarkers.push(facilityMarker);
-                }
-            } else {
-                alert('주변에 시설이 없습니다.');
-            }
-        },
-        error: function (request, status, error) {
-            console.error('주변 시설을 불러오는 중 에러 발생', error);
-            alert('주변 시설을 불러오지 못했습니다. 다시 시도해주세요.');
+  $.ajax({
+    url: "/facility/around",
+    method: "GET",
+    data: {
+      lat: lat,
+      lng: lng,
+    },
+    success: function (response) {
+      removeFacilityMarkers();
+      if (Array.isArray(response) && response.length > 0) {
+        for (var i = 0; i < response.length; i++) {
+          var facility = response[i];
+          var facilityLatlng = new naver.maps.LatLng(
+            facility.lat,
+            facility.lng
+          );
+          var facilityMarker = new naver.maps.Marker({
+            map: map,
+            position: facilityLatlng,
+            title: facility.name,
+          });
+          facilityMarkers.push(facilityMarker);
         }
-    });
-}
-// 기존 마커들을 모두 제거하는 함수
-function removeFacilityMarkers() {
-    for (var i = 0; i < facilityMarkers.length; i++) {
-        facilityMarkers[i].setMap(null);
-    }
-    facilityMarkers = [];
+      } else {
+        alert("주변에 시설이 없습니다.");
+      }
+    },
+    error: function (request, status, error) {
+      console.error("주변 시설을 불러오는 중 에러 발생", error);
+      alert("주변 시설을 불러오지 못했습니다. 다시 시도해주세요.");
+    },
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    var sidebar = document.querySelector('.sidebar');
-    var toggleButton = document.querySelector('.toggle-btn');
-    
-    toggleButton.addEventListener('click', function () {
-        // 사이드바의 상태를 토글합니다.
-        sidebar.classList.toggle('sidebar-collapsed');
-        
-        // 사이드바가 접힌 상태면 버튼도 왼쪽으로 이동합니다.
-        if (sidebar.classList.contains('sidebar-collapsed')) {
-            toggleButton.style.left = '0px'; // 버튼을 왼쪽으로 이동시킵니다.
-            toggleButton.textContent = '<'; // 버튼의 텍스트를 변경합니다.
-        } else {
-            toggleButton.style.left = '400px'; // 버튼을 원래 위치로 되돌립니다.
-            toggleButton.textContent = '>';
-        }
+function removeFacilityMarkers() {
+  for (var i = 0; i < facilityMarkers.length; i++) {
+    facilityMarkers[i].setMap(null);
+  }
+  facilityMarkers = [];
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var sidebar = document.querySelector(".sidebar");
+  var toggleButton = document.querySelector(".toggle-btn");
+
+  toggleButton.addEventListener("click", function () {
+    sidebar.classList.toggle("sidebar-collapsed");
+    if (sidebar.classList.contains("sidebar-collapsed")) {
+      toggleButton.style.left = "0px";
+      toggleButton.textContent = "<";
+    } else {
+      toggleButton.style.left = "400px";
+      toggleButton.textContent = ">";
+    }
+  });
+  selectMapList();
+});
+
+var modal = document.getElementById("myModal");
+
+var eduInstitude = document.getElementById("edInstitude");
+
+function openModal() {
+  modal.style.display = "block";
+}
+
+function showMarkers(key) {
+  var bounds = map.getBounds();
+  var center = bounds.getCenter();
+
+  console.log("key === ", key);
+
+  $.ajax({
+    url: "/facility/",
+    method: "GET",
+    data: {
+      lat: center.lat(),
+      lng: center.lng(),
+      type: key,
+    },
+    success: function (response) {
+      console.log("value : ", response);
+    },
+    error: function (request, status, error) {
+      console.error("주변 시설을 불러오는 중 에러 발생", error);
+      alert("주변 시설을 불러오지 못했습니다. 다시 시도해주세요.");
+    },
+  });
+  const arr = [
+    {
+      id: 118,
+      created_at: "2024-04-09T17:05:33.277331",
+      updated_at: "2024-04-09T17:05:33.277376",
+      name: "하당동 행정복지센터",
+      address: "전남 목포시 하당로 215",
+      type: "주민센터",
+      lat: 34.8086412384379,
+      lng: 126.420064965595,
+      imageUrl:
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210206_250%2F1612575043521ah9D6_JPEG%2FMfZhDYNAK4n0KZPidaoJgc_b.jpg",
+    },
+    {
+      id: 119,
+      created_at: "2024-04-09T17:05:33.277331",
+      updated_at: "2024-04-09T17:05:33.277376",
+      name: "하당동 행정복지센터",
+      address: "전남 목포시 하당로 215",
+      type: "주민센터",
+      lat: 34.80040081802326,
+      lng: 126.38475611606447,
+      imageUrl:
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210206_250%2F1612575043521ah9D6_JPEG%2FMfZhDYNAK4n0KZPidaoJgc_b.jpg",
+    },
+    {
+      id: 120,
+      created_at: "2024-04-09T17:05:33.277331",
+      updated_at: "2024-04-09T17:05:33.277376",
+      name: "하당동 행정복지센터",
+      address: "전남 목포시 하당로 215",
+      type: "주민센터",
+      lat: 34.790703,
+      lng: 126.395485,
+      imageUrl:
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210206_250%2F1612575043521ah9D6_JPEG%2FMfZhDYNAK4n0KZPidaoJgc_b.jpg",
+    },
+  ];
+
+  var iconUrl = `../static/assets/img/${key.replaceAll(" ", "")}.png`;
+
+  arr.forEach(function (item) {
+    var facilityMarker = new naver.maps.Marker({
+      map: map,
+      position: new naver.maps.LatLng(item.lat, item.lng),
+      title: item.name,
+      icon: {
+        url: iconUrl,
+      },
     });
+    facilityMarkers.push(facilityMarker);
+  });
+
+  $(".sideBarContainer").css("display", "block");
+
+  arr.forEach(function (place) {
+    var html = `
+      <div class="card">
+        <div class="card-body">
+          <div class="col-md-4">
+            <img src="${place.imageUrl}" alt="이미지">
+          </div>
+          <div id="place_intro">
+            <h5 class="card-title">${place.name}</h5>
+            <a class="card-subtitle">${place.type}</a>
+          </div>
+          <p class="card-text">${place.address}</p>
+        </div>
+      </div>
+      <hr />
+    `;
+
+    // Append the HTML to the side-list container
+    $(".side-list").append(html);
+  });
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = closeModal;
+  var categoryColors = {
+    medical: "#abf7b1",
+    education: "#fcfcdc",
+    facilities: "lightskyblue",
+    residential: "lightcoral",
+    leisure: "thistle",
+  };
+
+  document.querySelectorAll(".category").forEach(function (button) {
+    button.addEventListener("click", function () {
+      var category = this.getAttribute("data-category");
+      var detailsId = "details-" + category;
+      var arrow = document.getElementById("arrow");
+      var categoryButton = document.getElementById(category);
+
+      document.querySelectorAll(".category").forEach(function (category) {
+        category.style.backgroundColor = "";
+      });
+
+      categoryButton.style.backgroundColor = categoryColors[category];
+
+      document
+        .querySelectorAll(".detail-container")
+        .forEach(function (container) {
+          container.style.display = "none";
+        });
+
+      var detailsContainer = document.getElementById(detailsId);
+      detailsContainer.style.display = "flex";
+      arrow.style.display =
+        detailsContainer.style.display === "flex" ? "block" : "none";
+
+      detailsContainer.querySelectorAll(".detail").forEach(function (detail) {
+        detail.removeEventListener("click", handleDetailClick);
+
+        detail.addEventListener("click", handleDetailClick);
+      });
+    });
+  });
+
+  function handleDetailClick() {
+    var detailText = this.textContent.trim();
+    var isDuplicate = Array.from(
+      document.querySelectorAll("#selected-details .selected-detail-item")
+    ).some(function (item) {
+      return item.textContent.trim().indexOf(detailText) > -1;
+    });
+    var perfecthomes = document.getElementById("perfecthomes");
+    perfecthomes.style.display = "block";
+
+    if (!isDuplicate) {
+      addSelectedDetail(
+        this.textContent.trim(),
+        this.parentNode.getAttribute("id").replace("details-", "")
+      );
+    }
+  }
+
+  function addSelectedDetail(detailText, category) {
+    var detailEl = document.createElement("div");
+    detailEl.classList.add("selected-detail-item");
+    detailEl.textContent = detailText;
+    detailEl.style.backgroundColor = categoryColors[category];
+    var removeBtn = document.createElement("span");
+    removeBtn.textContent = "×";
+    removeBtn.onclick = function () {
+      detailEl.remove();
+    };
+    removeBtn.style.cursor = "pointer";
+    removeBtn.style.marginLeft = "10px";
+    detailEl.appendChild(removeBtn);
+
+    document.getElementById("selected-details").appendChild(detailEl);
+  }
+
+  function closeModal() {
+    modal.style.display = "none";
+
+    document.querySelectorAll(".category").forEach(function (category) {
+      category.style.backgroundColor = "";
+    });
+
+    var selectedDetailsContainer = document.getElementById("selected-details");
+    selectedDetailsContainer.innerHTML = "";
+
+    var detailContainers = document.querySelectorAll(".detail-container");
+    detailContainers.forEach(function (container) {
+      container.style.display = "none";
+    });
+
+    var arrow = document.getElementById("arrow");
+    arrow.style.display = "none";
+
+    var perfecthomes = document.getElementById("perfecthomes");
+    perfecthomes.style.display = "none";
+  }
 });
